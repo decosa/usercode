e3312ff11d748725c32f9aa6ea90a6b0471c7abd
decosa <>
2010-02-03 17:16:16 +0000

    new implementation for dumpPython

--- tools/emptyTools.py
+++ tools/emptyTools.py
@@ -1,5 +1,7 @@
 from FWCore.GuiBrowsers.ConfigToolBase import *
 
+path = "FWCore.GuiBrowsers.emptyTools"
+
 class ToolName(ConfigToolBase):
 
     """
@@ -7,6 +9,7 @@ class ToolName(ConfigToolBase):
     """
     _label='ToolName'
     _defaultParameters={}
+    _path = path 
     def __init__(self):
         ### import base class constructor
         ConfigToolBase.__init__(self)
@@ -20,18 +23,7 @@ class ToolName(ConfigToolBase):
         self._comment = ""
     def getDefaultParameters(self):
         return self._defaultParameters
-    def dumpPython(self):
-        ### returns the code to include in a config file to use the tool
-        ### the dumpPythonImport variable is the import line to add only if it has not already included
-        dumpPythonImport = "\nfrom PhysicsTools.PatAlgos.tools.trackTools import *\n"
-        dumpPython=''
-        if self._comment!="":
-            dumpPython = '#'+self._comment
-        dumpPython = "\ntoolName(process, "
-        dumpPython += '"'+str(self.getvalue('parName'))+'"'+", "
-        dumpPython += '"'+str(self.getvalue('parName2'))+'"'+")"+'\n'
-        return (dumpPythonImport,dumpPython) 
-
+   
     def __call__(self,process,
                  parName1     = None,
                  parName2     = None) :
@@ -44,27 +36,20 @@ class ToolName(ConfigToolBase):
         ### the setParameter method provides checks about type and about values (if supported ones are specified in addParameter - Range )
         self.setParameter('parName',parName)
         self.setParameter('parName2',parName)
-        ### apply tool by calling method apply
+        ### apply tool by calling method apply defined in the base class. i calls toolCode method.
         self.apply(process) 
         
-    def apply(self, process):
+    def toolCode(self, process):
+        
         ### rename parameter names just to avoid change the old code        
         parName=self._parameters['parName'].value
         parName2=self._parameters['parName2'].value
-        ### put the following two line to make tool working even outside the ConfigEditor
-        ### disableRecording() does not allow to store in the history tools called by the main one
-        if hasattr(process, "addAction"):
-            process.disableRecording()
-
-
+     
+      
         ### PUT HERE THE TOOL CODE
 
-        ### enableRecording() restores the history recording
-        if hasattr(process, "addAction"):
-            process.enableRecording()
-            ### copy the tool and add it to the history
-            action=self.__copy__()
-            process.addAction(action)
+       
+       
 
 ### instance of the tool class
 ### it allows the user to run old configuration files without change anything, 
