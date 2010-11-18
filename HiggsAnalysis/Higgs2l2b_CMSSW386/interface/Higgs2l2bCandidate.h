15a4d1d7a212dec0c88df1b7a9dc98b0a998d8ef
decosa <>
2010-11-18 11:06:31 +0000

    added analysis module - helicity studies

--- HiggsAnalysis/Higgs2l2b_CMSSW386/interface/Higgs2l2bCandidate.h
+++ HiggsAnalysis/Higgs2l2b_CMSSW386/interface/Higgs2l2bCandidate.h
@@ -5,18 +5,21 @@ struct Higgs2l2bCandidate {
 
         float zllmass, zllpt, zlleta, zllphi;
         float zjjmass, zjjpt, zjjeta, zjjphi;
+        float met, metsig, metphi;
         float higgsmass, higgspt, higgseta, higgsphi;
-        float lminpt, lminet, lmineta, lminphi, lminHLT;
-        float lmaxpt, lmaxet, lmaxeta, lmaxphi, lmaxHLT;
+        float lminpt, lmineta, lminphi;
+        float lmaxpt, lmaxeta, lmaxphi;
         float jminpt, jmineta, jminphi;
         float jmaxpt, jmaxeta, jmaxphi;
         float jmincsv, jmincsvmva, jminjprob, jminjbprob, jminssvhe, jminssvhp, jminelpt, jminelip, jminmu, jminmupt, jminmuip, jmintkhe, jmintkhp;
         float jmaxcsv, jmaxcsvmva, jmaxjprob, jmaxjbprob, jmaxssvhe, jmaxssvhp, jmaxelpt, jmaxelip, jmaxmu, jmaxmupt, jmaxmuip, jmaxtkhe, jmaxtkhp;
-        float numel, nummu, numjet;
+        bool  jminbmatch, jmincmatch;
+        bool  jmaxbmatch, jmaxcmatch;
+        bool  jminid, jmaxid;
         float zzdphi, zzdeta, zzdr;
         float lldphi, lldeta, lldr;
         float jjdphi, jjdeta, jjdr;    
-        bool isMuon;
+        bool  isMuon;
 
 };
 
