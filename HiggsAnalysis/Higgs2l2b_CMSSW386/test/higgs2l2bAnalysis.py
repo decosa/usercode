8932a0a032446e0317a57a37e7d84037eec678c5
decosa <>
2011-01-25 16:35:48 +0000

    included EventCounter analyzer

--- HiggsAnalysis/Higgs2l2b_CMSSW386/test/higgs2l2bAnalysis.py
+++ HiggsAnalysis/Higgs2l2b_CMSSW386/test/higgs2l2bAnalysis.py
@@ -7,18 +7,22 @@ process = cms.Process("TRIM")
 process.load("HiggsAnalysis.Higgs2l2b.Higgs2l2bedmNtuples_cff")
 #process.load("HiggsAnalysis.Higgs2l2b.zjjEdmNtuples_cff")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 
 process.TFileService = cms.Service("TFileService", fileName = cms.string('h2l2b300GF.root') )
 
 process.source = cms.Source("PoolSource")
 
-process.source.fileNames=cms.untracked.vstring('file:filename.root')
+process.source.fileNames=cms.untracked.vstring('rfio:/castor/cern.ch/user/d/decosa/Higgs/h350/skim/h2l2b300GF_1_1_Ukh.root')
 ###
 process.elChannelAnalysis = cms.EDAnalyzer("Higgs2l2bAnalysis",
     higgsTag = cms.InputTag("hzzeejj:h"),
 )
+process.elChannelEvtCounter = cms.EDAnalyzer("EventCounter",
+    higgsTag = cms.InputTag("hzzeejj:h"),
+    metTag = cms.InputTag("hzzeejj:met")
 
+)
 process.muChannelAnalysis = cms.EDAnalyzer("Higgs2l2bAnalysis",
     higgsTag = cms.InputTag("hzzmmjj:h"),
 )
@@ -32,6 +36,7 @@ process.edmNtuplesOut.fileName = cms.untracked.string('H300GFEdmNtuples.root')
 
 process.analysisPath = cms.Path(
     process.elChannelAnalysis+
+    process.elChannelEvtCounter+
     process.muChannelAnalysis+
     process.Higgs2e2bEdmNtuple+
     process.Higgs2mu2bEdmNtuple#+
