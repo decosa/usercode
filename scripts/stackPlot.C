15c8e873ea3287f32e172f7385ff123a59a335ef
decosa <>
2011-02-09 13:15:13 +0000

    scripts to getHistos, make stack plots and get number of events after progressive cuts

--- scripts/stackPlot.C
+++ scripts/stackPlot.C
@@ -1,4 +1,3 @@
-
 {
 gROOT->Reset();
 gROOT->SetStyle("Plain");
@@ -13,61 +12,102 @@ gROOT->SetStyle("Plain");
 
 // Z->mumu sample
 
-TFile * h_file = new TFile("h350.root","read");
-TH1D * h = (TH1D*) h_file->Get("HMassdrjjSel");
- for(int i=0; i<h->GetNbinsY();++i){
-   h->SetBinError(i,0);
-     }
-
-TFile * tt_file = new TFile("TT.root","read");
-TH1D * tt = (TH1D*) tt_file->Get("HMassdrjjSelmu");
-
-
-TFile * zz_file = new TFile("ZZ.root","read");
-TH1D * zz = (TH1D*) tt_file->Get("HMassdrjjSelmu");
-
-TFile * wz_file = new TFile("WZ.root","read");
-TH1D * wz = (TH1D*) tt_file->Get("HMassdrjjSelmu");
+ TCanvas c1;
+ // TH1F * h = new TH1F("h", "M_{H} after cut on #Delta r_{jj}", 200,0,1000);
+ TFile * h_file = TFile::Open("h350.root", "READ");
+ TH1 * h_original = (TH1*) h_file->Get("HMassdrjjSel");
+ TH1 * h = h_original->Clone("h");
+ h->Rebin(5); 
+ h->Draw();
 
 
- string bkg[]={"Z1Jet","Z2Jet","Z3Jet","Z4Jet","Z5Jet",};
- TH1D * zjet = new TH1D("HMassdrjj", "M_{H} after cut on #Delta r_{jj}", 20,200,600);
+ TH1F * tt = new TH1F("tt", "M_{H} after cut on #Delta r_{jj}", 200,0,1000);
+ TFile * tt_file = new TFile("TT.root");
+ TH1 * tt_original = (TH1*) tt_file->Get("HMassdrjjSelmu");
+ tt->Add(tt_original);
+ tt->Rebin(5);
+ 
 
- for(int i=0;i<2;++i){
-   TFile * f = new TFile((bkg[i]+".root").c_str(),"read");
-   TH1D * b = (TH1D*) f->Get("HMassdrjjSelmu");
-   zjet->SetBins(b->GetNbinsX(), b->GetXaxis()->GetXmin(),b->GetXaxis()->GetXmax() );
+ TH1F * zz = new TH1F("zz", "M_{H} after cut on #Delta r_{jj}", 200,0,1000);
+ TFile * zz_file = new TFile("ZZ.root");
+ TH1 * zz_original = (TH1*)zz_file->Get("HMassdrjjSelmu");
+ cout<<"n bins zz pre rebin"<<zz->GetNbinsX()<<endl;
+ zz->Add(zz_original);
+ zz->Rebin(5);
+ cout<<"n bins zz post rebin"<<zz->GetNbinsX()<<endl;
 
+ TH1F * wz = new TH1F("wz", "M_{H} after cut on #Delta r_{jj}", 200,0,1000);
+ TFile * wz_file = new TFile("WZ.root");
+ TH1 * wz_original = (TH1*) wz_file->Get("HMassdrjjSelmu");
+ wz->Add(wz_original);
+ wz->Rebin(5);
+
+
+ TH1F * zjet = new TH1F("HMassdrjj", "M_{H} after cut on #Delta r_{jj}", 200,0,1000);
+
+ // string bkg[24]; 
+ //string bkg[] ={"Z0Jet","Z1Jet","Z2Jet","Z3Jet","Z4Jet","Z5Jet","Z1Jet100_300","Z2Jet100_300","Z3Jet100_300","Z1Jet300_800","Z2Jet300_800","Z3Jet300_800","Z4Jet300_800","Z5Jet300_800","ZBB0","ZBB1", "ZBB2", "ZBB3","ZCC0", "ZCC2", "ZCC3"};
+
+ string bkg[] ={"Z0Jet","Z1Jet","Z2Jet","Z3Jet","Z4Jet","Z5Jet","Z1Jet100_300","Z2Jet100_300","Z3Jet100_300","Z4Jet100_300","Z5Jet100_300","Z1Jet300_800","Z2Jet300_800","Z3Jet300_800","Z4Jet300_800","Z5Jet300_800","ZBB0","ZBB1", "ZBB2", "ZBB3","ZCC0", "ZCC2", "ZCC3"};
+
+
+ 
+ const unsigned int nFiles = sizeof(bkg)/sizeof(string);
+ TFile * files[nFiles];
+ cout<<"array size "<<nFiles<<endl;
+ for(int i=0;i<nFiles;++i){
+   files[i] = TFile::Open((bkg[i]+".root").c_str(), "READ");
+   TH1 * b = (TH1*) files[i]->Get("HMassdrjjSelmu");
+   //cout<<"file: "<< bkg[i]+".root"<<endl;
    zjet->Add(b);
+   //cout<<"t: "<<zjet->Integral(0,1000)<<endl;
 }
+ 
 
 
-TH1D * S0 = new TH1D("S0","H mass",20,200,600);
-S0->SetStats(kFALSE);
-S0->SetXTitle("m_{H} (GeV/c^{2})");
-S0->SetYTitle("Events/5 GeV/c^{2}");
- S0->SetMinimum(0.5);
- S0->GetYaxis()->SetRangeUser(0,0.4);
-S0->GetXaxis()->SetTitleSize(0.045);
-S0->GetXaxis()->SetTitleOffset(0.95);
-S0->GetXaxis()->SetLabelSize(0.045);
-S0->GetYaxis()->SetTitleOffset(0.9);
-S0->GetYaxis()->SetLabelSize(0.045);
-S0->GetYaxis()->SetTitleSize(0.05);
+ zjet->Rebin(5);
+ zjet->Draw("HIST");
 
- TH1D * S1 = new TH1D(*tt);
- S1->SetLineColor(kBlue);
- TH1D * S12 = new TH1D(* S1); S12->Add(zz);
- S12->SetLineColor(kGreen);
- TH1D * S123 = new TH1D(* S1); S123->Add(wz);
- S123->SetLineColor(kYellow);
- TH1D * S1234 = new TH1D(* S1); S1234->Add(zjet);
- S1234->SetLineColor(kViolet);
- TH1D * S12345 = new TH1D(* S1); S12345->Add(h);
- S12345->SetLineColor(kMagenta);
+
+ TH1F * S0 = new TH1F("S0","H mass",200,0,1000); 
+ S0->Rebin(5);
+ S0->SetStats(kFALSE);
+ S0->SetXTitle("m_{H} (GeV/c^{2})");
+ S0->SetYTitle("Events/5 GeV/c^{2}");
+ //S0->SetMinimum(0.5);
+ S0->GetYaxis()->SetRangeUser(0,1.);
+ S0->GetXaxis()->SetTitleSize(0.045);
+ S0->GetXaxis()->SetTitleOffset(0.95);
+ S0->GetXaxis()->SetLabelSize(0.045);
+ S0->GetYaxis()->SetTitleOffset(0.9);
+ S0->GetYaxis()->SetLabelSize(0.045);
+ S0->GetYaxis()->SetTitleSize(0.05);
+
+  cout<<"n bins zz "<<zz->GetNbinsX()<<endl;
+  //S1->SetBins(20, 200,600 );
+
+  
+
+  cout<<"n bins zjet "<<zjet->GetNbinsX()<<endl;
+
+
+
+
+ // zz->Draw();
+ TH1F * S1 = new TH1F(*wz);
+ TH1F * S12 = new TH1F(* S1);
+ S12->Add(zz);
+ TH1F * S123 = new TH1F(* S12); 
+ S123->Add(tt);
+ TH1F * S1234 = new TH1F(* S123); 
+ S1234->Add(zjet);
+ TH1F * S12345 = new TH1F(* S1234);
+ S12345->Add(h);
+
+
 //TH1D * S12345 = new TH1D(* S1234); S12345->Add(wwToMuMu_draw);
 //S12345->SetLineColor(kMagenta);
-//TH1D * S12345 = new TH1D(* S1); S12345->Add(zToMuMu);
+//TH1D * S12345 = new TH1Dnnnn(* S1); S12345->Add(zToMuMu);
 //S12345->SetLineColor(kRed);
 
 //S12345->SetStats(kFALSE);
@@ -112,25 +152,29 @@ S0->GetYaxis()->SetTitleSize(0.05);
 
 
 
-// S12345->SetMarkerStyle(0);
+ S12345->SetMarkerStyle(0);
 
  S0->Draw("HIST");
-// S12345->Draw("HSAME");
  
- S12345->Draw("HISTSAME");
+ 
+ //S12345->Draw("HISTSAME");
  S1234->Draw("HISTSAME");
  S123->Draw("HISTSAME");
  S12->Draw("HISTSAME");
  S1->Draw("HISTSAME");
-
+ h->SetLineColor(kBlack);
+ h->SetFillColor(0);
+ h->SetLineWidth(2);
+ h->Draw("HISTSAME");
 
  leg = new TLegend(.75,.75,1.0,1.0);
  leg->SetFillColor(0);
- leg->AddEntry(S1,"t #bar{t}","f");
+ leg->AddEntry(S1,"WZ","f");
  leg->AddEntry(S12,"ZZ","f");
- leg->AddEntry(S123,"WZ ","f");
- leg->AddEntry(S1234,"Zjets","f");
- leg->AddEntry(S12345,"H350","f");
+ leg->AddEntry(S123,"t #bar{t}","f");
+ leg->AddEntry(S1234,"ZJets ","f");
+ // leg->AddEntry(S12345,"H350","f");
+ leg->AddEntry(h,"H350","l");
 
 
 //leg->AddEntry(S12345,"WW inclusive","f");
@@ -139,15 +183,15 @@ S0->GetYaxis()->SetTitleSize(0.05);
 
  leg->Draw("SAME");
  S1->SetFillColor(kTeal+5);
- S1->SetMarkerColor(kTeal+3);
+ S1->SetLineColor(kTeal+3);
  S12->SetFillColor(kMagenta+3);
- S12->SetMarkerColor(kMagenta+4);
+ S12->SetLineColor(kMagenta+4);
  S123->SetFillColor(kViolet+4);
- S123->SetMarkerColor(kViolet+3);
+ S123->SetLineColor(kViolet+3);
  S1234->SetFillColor(kOrange-2);
- S1234->SetMarkerColor(kOrange+8);
+ S1234->SetLineColor(kOrange+8);
  S12345->SetFillColor(kPink-3);
- S12345->SetMarkerColor(kPink-7);
+ S12345->SetLineColor(kPink-7);
 
 
 //S12345->SetFillColor(kMagenta);
@@ -157,7 +201,10 @@ S0->GetYaxis()->SetTitleSize(0.05);
  c1->SetTicky(0);
 
 //c1->SaveAs("zmumu_sel.eps");
-
-
+//  zz_file->Close();
+//  tt_file->Close();
+//  wz_file->Close();
+//  h_file->Close();
+//  for(int i=0;i<nFiles;++i) files[i]->Close();
 
 }
