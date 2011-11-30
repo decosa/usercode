7db717d8f5538fa22aaf0fdaac591c2d29e0bfba
decosa <>
2011-11-30 11:51:09 +0000

    added syst on lumi

--- scripts/makeDataCards_2l2b.py
+++ scripts/makeDataCards_2l2b.py
@@ -1,11 +1,5 @@
 
-### This scripts provides datacards to run a limit.
-### It takes in input txt reports files produced by plot.C,
-### produces datacards for each mass point and for each channel,
-### combines datacards for different channels and same category
-
-
-
+### This scripts provides datacards to run a limit. It takes in input txt reports files produced by plot.C mac
 
 import os, commands
 
@@ -13,11 +7,12 @@ import os, commands
 def makeDatacards(category):
 
 
-    filename = "lljjmass_"+category+".txt"
+    filename = "lljjmass"+category+".txt"
     if(category=="2btag"):
         filename = "lljjmass.txt"
     name = "_"+category+"_datacard.txt"
 
+#    m = [ "250"]
     m = [ "250", "300", "350","400", "450", "500"]
 
     for mass in m:
@@ -34,7 +29,7 @@ def makeDatacards(category):
         muChanBin = "mm"+category
         elChanBin = "ee"+category
         #kmax 0 number of nuisance parameters (sources of systematical uncertainties) "
-        newfile.write('imax 2  number of channels\njmax 1  number of backgrounds\nkmax 0  number of nuisance parameters \n')
+        newfile.write('imax 2  number of channels\njmax 1  number of backgrounds\nkmax 1  number of nuisance parameters \n')
         newfile.write("------------\n")
         newfile.write("bin            "+muChanBin+"   "+elChanBin+"\n")
         
@@ -74,7 +69,8 @@ def makeDatacards(category):
         newfile.write("------------\n")
         newfile.write("bin            "+muChanBin+"   "+muChanBin+"   "+elChanBin+"   "+elChanBin+"\nprocess        sig    bg    sig   bg\nprocess         0      1     0     1\n")
         newfile.write("rate          "+str(musig)+"  "+str(mubkg)+"  "+str(elsig)+"  "+str(elbkg)+"  "+"\n")
-
+        newfile.write("------------\n")
+        newfile.write("lumi  lnN     1.045    1.045   1.045    1.045  \n")
 
 category = ["0btag","1btag", "2btag"]
 
@@ -89,5 +85,18 @@ for m in mass:
         cmd += m+ "_"+c+"_datacard.txt "
     cmd += " > "+m+"_datacard.txt"
     os.system(cmd)
+#    if (line.startswith("")): elChannel=True
 
+
+    #newline = string.replace(line, "/Freq_CLs_grid_ts3_comb_hgg.root", "/Freq_CLs_grid_ts3_comb_hgg_"+str(n)+".root")
+    #newfile.write(newline)
+    
+ #   cmd = "chmod 775 "+ filename+"_"+str(n)+ext
+  #  os.system(cmd)
+  #  cmd = "bsub -q 8nh "+ filename+"_"+str(n)+ext
+    #print cmd
+   # os.system(cmd)
+    
+    #cmd = "bjobs"
+   # os.system(cmd)
                                                                             
